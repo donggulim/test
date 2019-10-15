2437e2481f7a3dcd173398b81f0859ac623a1a92
donggulim <github ehdrndia@naver.com>
2019-10-15 11:08:48 +0900

    abc

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var el = document.querySelector(".outside");
 
-el.addEventListener("click",function(){
-    console.log('click!',e);
-
+el.addEventListener("click",function(e){
+    var target = e.target;
+    console.log(target.className, target.nodeName);
 });
